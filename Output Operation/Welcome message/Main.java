71b9b060336989f457bdc9a46994147dce038468
AdelleStuuu~ <adelledoesstuff@gmail.com>
2026-01-09 23:52:09 +0800

    Hello World Program

---
+++ Output Operation/Welcome message/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+
+    public static void main(String[] args) {
+        // Hey there, start typing your Java code here...
+        System.out.println("Hello, World!");
+    }
+}
