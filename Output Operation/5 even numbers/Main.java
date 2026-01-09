91b3a03ce24c13dcea7b45df5d031175a5297284
AdelleStuuu~ <adelledoesstuff@gmail.com>
2026-01-09 23:52:59 +0800

    Program that outputs the first 5 even numbers

---
+++ Output Operation/5 even numbers/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+
+    public static void main(String[] args) {
+        // Hey there, start typing your Java code here...
+        System.out.println("2\n4\n6\n8\n10");
+    }
+}
