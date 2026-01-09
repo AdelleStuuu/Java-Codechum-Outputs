477462470663c1071c406762cf60bc69a8dc7579
AdelleStuuu~ <adelledoesstuff@gmail.com>
2026-01-09 23:51:31 +0800

    program that spells out the word code with each big letter being an upper case ascii art of it

---
+++ Output Operation/Code in Code/Main.java
@@ -0,0 +1,9 @@
+public class Main{
+    public static void main(String[] args) {
+        System.out.println("CCCCC   OOOOO   DDDDD   EEEEE");
+        System.out.println("C       O   O   D   D   E");
+        System.out.println("C       O   O   D   D   EEE");
+        System.out.println("C       O   O   D   D   E");
+        System.out.println("CCCCC   OOOOO   DDDDD   EEEEE");
+    }
+}
