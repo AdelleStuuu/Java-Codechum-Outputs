bfe220817a7a6b6e44d81f55584e95bdf61a667c
AdelleStuuu~ <adelledoesstuff@gmail.com>
2026-01-09 23:58:08 +0800

    A fake database program that makes use of escape sequences

---
+++ Escape Sequences/Database Table/Main.java
@@ -0,0 +1,10 @@
+public class Main{
+    public static void main(String[] args) {
+        System.out.println("Database Record");
+        System.out.println("\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\");
+        System.out.println("Name:\t\tJohn Doe");
+        System.out.println("Email:\t\tjohn.doe@example.com");
+        System.out.println("University:\tABC University");
+        
+    }
+}
