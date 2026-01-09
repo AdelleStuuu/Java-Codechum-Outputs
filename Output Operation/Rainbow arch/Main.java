531c0964bcb342071cb426dc0ce2e22f305ecaee
AdelleStuuu~ <adelledoesstuff@gmail.com>
2026-01-09 23:50:12 +0800

    program that has an arch with the words of colors

---
+++ Output Operation/Rainbow arch/Main.java
@@ -0,0 +1,8 @@
+public class Main{
+    public static void main(String[] args) {
+        System.out.println("            Green");
+        System.out.println("      Yellow     Brown");
+        System.out.println("  Orange             Indigo");
+        System.out.println("Red                      Violet");
+    }
+}
