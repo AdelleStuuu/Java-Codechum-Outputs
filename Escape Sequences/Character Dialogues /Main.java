a0e477ced5024586191718f1285012e250be6ac1
AdelleStuuu~ <adelledoesstuff@gmail.com>
2026-01-09 23:55:23 +0800

    Program that has dialogues that made use of the quotation sequences

---
+++ Escape Sequences/Character Dialogues /Main.java
@@ -0,0 +1,14 @@
+public class Main{
+    public static void main(String[] args) {
+        System.out.println("Character A:	\"Hello there!\"");
+        System.out.println("Character B:	'Hi! How are you?'");
+        System.out.println("Character A:	'I'm doing great.' Thanks for asking!");
+        System.out.println("Character C:	\"Hey, what\'s going on?\"");
+        System.out.println("Character B:	\"We were just catching up.\"");
+        System.out.println("Character A:	\"Yeah, it's been a while.\"");
+        System.out.println("Character C:	\"Nice to see you both!\"");
+        System.out.println("Character B:	\"Likewise! How have you been?\"");
+        System.out.println("Character C:	\"I've been good. Thanks!\"");
+        System.out.println("Character A:	\"That's great to hear.\"");
+    }
+}
