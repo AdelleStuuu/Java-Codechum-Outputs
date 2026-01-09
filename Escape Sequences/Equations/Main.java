8e60366a17ac465d9d4ef3ae081cff2053b80a4a
AdelleStuuu~ <adelledoesstuff@gmail.com>
2026-01-09 23:57:19 +0800

    Program that uses the tab sequence to output a mathematical table

---
+++ Escape Sequences/Equations/Main.java
@@ -0,0 +1,11 @@
+public class Main{
+    public static void main(String[] args) {
+        System.out.println("Equations:");
+        System.out.println("5\t+\t3\t=\t8");
+        System.out.println("12\t-\t7\t=\t5");
+        System.out.println("4\t*\t6\t=\t24");
+        System.out.println("20\t/\t5\t=\t4");
+        System.out.println("10\t%\t3\t=\t1");
+        
+    }
+}
