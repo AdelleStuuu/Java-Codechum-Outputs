00273a1d7ea9f999bd8f020bf3d32d0c7013e2fc
AdelleStuuu~ <adelledoesstuff@gmail.com>
2026-01-10 17:52:00 +0800

    Program that asks for personal information and provides an output

---
+++ Java Lessons/Fundamentals/Input Operation/Personal Information/Main.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class Main {
+
+    public static void main(String[] args) {
+        // Hey there, start typing your Java code here...
+        Scanner scan = new Scanner(System.in);
+        
+        System.out.print("Enter your name: ");
+        var name = scan.nextLine();
+        System.out.print("Enter your age: ");
+        var age = scan.nextLine();
+        System.out.print("Enter your city: ");
+        var city = scan.nextLine();
+        System.out.println("Name: "+name);
+        System.out.printf("Age: %s\n",age);
+        System.out.printf("City: %s\n",city);
+
+        scan.close();
+    }
+}
