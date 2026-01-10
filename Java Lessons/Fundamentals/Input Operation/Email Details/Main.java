0e8b5453c309f6bbbd89537053c0107890c7c5df
AdelleStuuu~ <adelledoesstuff@gmail.com>
2026-01-10 17:53:06 +0800

    Supressing quick warnings

--- Java Lessons/Fundamentals/Input Operation/Email Details/Main.java
+++ Java Lessons/Fundamentals/Input Operation/Email Details/Main.java
@@ -1,6 +1,7 @@
 import java.util.Scanner;
 public class Main {
 
+    @SuppressWarnings("ConvertToTryWithResources")
     public static void main(String[] args) {
         // Hey there, start typing your Java code here...
         Scanner scan = new Scanner(System.in);
