6ff7690b4b77eddab9f47e621ad88ee3c04a0f5b
AdelleStuuu~ <adelledoesstuff@gmail.com>
2026-01-10 17:50:42 +0800

    A program that asks for a name and provides an output

---
+++ Java Lessons/Fundamentals/Input Operation/Name Input/Main.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class Main {
+
+    public static void main(String[] args) {
+        // Hey there, start typing your Java code here...
+        Scanner scan = new Scanner(System.in);
+        
+        System.out.print("Enter your name: ");
+        var name = scan.nextLine();
+        System.out.printf("Welcome, %s!",name);
+
+        scan.close();
+    }
+}
